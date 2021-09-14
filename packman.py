168a55a1ffeb5438843a0012a5f2b483d1873ecb
KarenRdzHdz <A01197734@itesm.mx>
2021-09-14 12:03:11 -0500

    Add more ghosts

--- packman.py
+++ packman.py
@@ -34,6 +34,11 @@ ghosts = [
     [vector(-180, -160), vector(0, 5)],
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
+
+    [vector(100, 80), vector(5, 0)],
+    [vector(-180, 80), vector(5, 0)],
+    [vector(-180, -80), vector(5, 0)],
+    [vector(100, -80), vector(5, 0)],
 ]
 # fmt: off
 tiles = [
